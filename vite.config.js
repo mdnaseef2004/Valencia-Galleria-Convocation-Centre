d9b0f449a604572723ab0e81e7ade0ae64559866
mdnaseef2004 <mdnaseef2004@users.noreply.github.com>
2026-08-31 18:00:05 +0530

    Fix GitHub Pages missing images by adding automatic dist image copy plugin and .nojekyll

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,49 @@
 import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
+
+function copyImageDirsPlugin() {
+  return {
+    name: 'copy-image-dirs',
+    closeBundle() {
+      const dirsToCopy = [
+        'Extertior',
+        'Operation Director',
+        'Ayadi-Convocation-Event',
+        'rahull ghandhi',
+        'Alhambra',
+        'Andalisia',
+        'Build-X-Event',
+        'CONFERENCES',
+        'Cordoba-Granada',
+        'Interior',
+        'Raihan-Madrasa-Event',
+        'Spark-Connect-Event',
+        'The First Impression',
+        'Zeely-Convocation-Event',
+        'logo'
+      ];
+
+      const distDir = path.resolve(__dirname, 'dist');
+      if (!fs.existsSync(distDir)) {
+        fs.mkdirSync(distDir, { recursive: true });
+      }
+
+      // Create .nojekyll in dist so GitHub Pages serves all files
+      fs.writeFileSync(path.join(distDir, '.nojekyll'), '');
+
+      dirsToCopy.forEach(dir => {
+        const srcDir = path.resolve(__dirname, dir);
+        const destDir = path.join(distDir, dir);
+        if (fs.existsSync(srcDir)) {
+          fs.cpSync(srcDir, destDir, { recursive: true });
+        }
+      });
+    }
+  };
+}
 
 export default defineConfig({
   base: './',
+  plugins: [copyImageDirsPlugin()],
 });
